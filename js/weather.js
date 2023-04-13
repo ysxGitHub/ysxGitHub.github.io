49fb42dfba25132d50e02dff3ce1422c32db3d81
ysxGitHub <603520880@qq.com>
2023-04-13 22:28:38 +0800

    Site updated: 2023-04-13 22:28:37

---
+++ js/weather.js
@@ -0,0 +1,22 @@
+WIDGET = {
+    "CONFIG": {
+      "modules": "01234",
+      "background": "5",
+      "tmpColor": "000000",
+      "tmpSize": "16",
+      "cityColor": "000000",
+      "citySize": "16",
+      "aqiColor": "000000",
+      "aqiSize": "16",
+      "weatherIconSize": "24",
+      "alertIconSize": "18",
+      "padding": "15px 10px 10px 20px",
+      "shadow": "0",
+      "language": "auto",
+      "borderRadius": "5",
+      "fixed": "false",
+      "vertical": "top",
+      "horizontal": "left",
+      "key": "44dccc23dabd4f47a03bdf7bf5c466e3"
+    }
+}
